d26b95e8518baa5dfb88767b2cf115feab287138
MatiasDeVincenti <mdevinceti@etec.uba.ar>
2024-09-16 16:21:54 -0300

    Agregué la autorización de GitHub

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import Header from "./components/Header";
 import Post from "./components/Post";
 import Footer from "./components/Footer";
+import { supabase } from "./live/helper/supaBaseClient";
 
 function App() {
   const [user, setUser] = useState(null);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const getSession = async () => {
+      const { data, error } = await SupabaseAuthClient.auth.getSession();
+      if (error) {
+        console.log(error);
+      } else {
+        setUser(data?.session?.user);
+      }
+    };
+    getSession();
+  }, []);
+
+  const handleLogin = async () => {
+    const { error, data } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+    });
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  };
 
   return (
     <>
       <Header />
+      <button onClick={handleLogin}>Iniciar sesión con GitHub</button>
       <Post
         title={"Titulo de prueba"}
         link={"https://www.hipocampo.org/FototestOnline/Fototest.jpg"}
--- Blog/src/live/helper/supaBaseClient.js
+++ Blog/src/live/helper/supaBaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
+export const supabase = createClient(
   "https://mxeeldheotieekeytfzn.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14ZWVsZGhlb3RpZWVrZXl0ZnpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MDQ2ODgsImV4cCI6MjA0MTQ4MDY4OH0.JKylv86gglGA3eO-PrP5VgICwyBjY_OtCn4LTeK5r94"
 );
